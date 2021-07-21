db8968a16d0e9f72bc7dad1b42016d14fa34a3c5
buka69 <ramazanov.iv@gmail.com>
2021-07-21 14:25:45 +0300

    fix: поправил проверку

--- routes/movies.js
+++ routes/movies.js
@@ -12,7 +12,7 @@ router.post('/', auth, celebrate({
   body: Joi.object().keys({
     country: Joi.string().min(2).max(30).required(),
     director: Joi.string().min(2).max(30).required(),
-    duration: Joi.number(),
+    duration: Joi.number().required(),
     year: Joi.string().min(2).max(30).required(),
     description: Joi.string().min(2).max(30).required(),
     image: Joi.string().pattern(link).required(),
@@ -20,7 +20,7 @@ router.post('/', auth, celebrate({
     thumbnail: Joi.string().pattern(link).required(),
     nameRU: Joi.string().min(2).max(30).required(),
     nameEN: Joi.string().min(2).max(30).required(),
-    movieId: Joi.number(),
+    movieId: Joi.number().required(),
   }),
 }), createCard);
 router.delete('/:_id', auth, celebrate({
